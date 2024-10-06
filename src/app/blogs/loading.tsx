fdb99ead2b85645a3f176318d2349a7ca633c8e9
Rahad-Ullah <myemail.abc20@gmail.com>
2024-10-07 00:23:28 +0600

    Custom loading page implemented

---
+++ src/app/blogs/loading.tsx
@@ -0,0 +1,13 @@
+const BlogLoadingPage = () => {
+  return (
+    <div>
+      <div className="">
+        <h1 className="text-4xl text-red-500 font-bold text-center my-10">
+          Loading...
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default BlogLoadingPage;
